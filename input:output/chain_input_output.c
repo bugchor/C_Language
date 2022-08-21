bb80772cc3652733d64eb11a0e2aff33202a7971
AMIT RAJ <88727915+bugchor@users.noreply.github.com>
2022-08-21 23:33:49 +0530

    Add files via upload

---
+++ input:output/chain_input_output.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+  int a,b,c;
+  printf("enter three no: ");
+  scanf("%d %d %d",&a,&b,&c);
+  printf("the first value is %d\nthe second value id %d\nthird value is %d",a,b,c);
+  return 0;
+}
---
+++ input:output/standard_input.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+  int a;
+   printf("enter the value: ");
+   scanf("%d",&a);//%d for int ,%f for float , %c for char
+   printf("the value added is %d",a);
+  return 0;
+}
